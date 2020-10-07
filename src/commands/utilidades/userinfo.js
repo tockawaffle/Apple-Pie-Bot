c4b63608b8e04a637ddffe5e7b0317985e035848
The-Crow-pleb <64552456+The-Crow-pleb@users.noreply.github.com>
2020-10-07 09:57:09 -0700

    Feitas mudanças no modo de execução

--- src/commands/utilidades/userinfo.js
+++ src/commands/utilidades/userinfo.js
@@ -1,23 +1,51 @@
 module.exports = {
     run: async(client, message, args) => {
-        const user = message.mentions.users.first() || message.author;
-        const member = message.mentions.members.first() || message.member;
+
+        const user = message.mentions.users.first()
         const { MessageEmbed } = require('discord.js');
         const { guild } = message;
-        if(!member) return message.channel.send('T1');
+        const moment = require('moment')
 
-        let embed = new MessageEmbed()
-            .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
-            .setDescription("Informações quentinhas chegando!")
-            .setColor("RANDOM")
-            .addField("Nome do Usuário:", user.tag)
-            .addField("ID:", user.id)
-            .addField("Conta criada em:", user.createdAt)
-            .addField("Status:", user.presence.status)
-            .addField("O que o(a) usuário(a) faz?:", user.presence.game ? user.presence.game : 'Não está fazendo nada!')
-        message.channel.send(embed);
-        console.log(embed)
 
+        if(!user) {
+
+            try {
+            
+                const memberId = message.content.substring(message.content.indexOf(' ') + 1)
+                const member = message.guild.members.cache.get(memberId);
+                const created = moment(member.createdAt).locale('pt-br').format('LLLL')
+
+                const embed = new MessageEmbed()
+                    .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
+                    .setDescription("Informações quentinhas chegando!")
+                    .setThumbnail(member.user.avatarURL({dynamic: true}))
+                    .setColor("RANDOM")
+                    .addField("Nome do Usuário:", member.user.tag)
+                    .addField("ID:", member.user.id)
+                    .addField("Conta criada em:", created)
+                    .addField("Status:", member.user.presence.status)
+                    .addField("O que o(a) usuário(a) faz?:", member.user.presence.game ? member.user.presence.game : 'Não está fazendo nada!')
+                message.channel.send(embed);
+
+            }catch(err) {
+                console.log(err)
+            }
+        } else {
+
+            const created = moment(user.createdAt).locale('pt-br').format('LLLL')
+
+            const embed = new MessageEmbed()
+                .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
+                .setDescription("Informações quentinhas chegando!")
+                .setThumbnail(user.avatarURL({dynamic: true}))
+                .setColor("RANDOM")
+                .addField("Nome do Usuário:", user.tag)
+                .addField("ID:", user.id)
+                .addField("Conta criada em:", created)
+                .addField("Status:", user.presence.status)
+                .addField("O que o(a) usuário(a) faz?:", user.presence.game ? user.presence.game : 'Não está fazendo nada!')
+            message.channel.send(embed);
+        }
     },
     aliases: ["uf"],
     description: 'Userinfo'
