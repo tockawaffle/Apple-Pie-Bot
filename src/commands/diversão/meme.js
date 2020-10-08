6234d09dabe0ca518cdfabae1c1bf1bafe809a57
The-Crow-pleb <64552456+The-Crow-pleb@users.noreply.github.com>
2020-10-08 10:07:22 -0700

    Comando de memes do Reddit

---
+++ src/commands/diversão/meme.js
@@ -0,0 +1,20 @@
+module.exports = {
+    run: async(client, message, args) => {
+        const { MessageEmbed } = require('discord.js')
+        const randomPuppy = require('random-puppy')
+
+        const subReddits = ["shitpostcrusaders", "terriblefacebookmemes", "DiretoDoZapZap", "g1comments", "Animemes"];
+        const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+
+        const img = await randomPuppy(random);
+        const embed = new MessageEmbed()
+            .setColor("RANDOM")
+            .setImage(img)
+            .setTitle(`Meme quentinho vindo de /r/${random}`)
+            .setURL(`https://reddit.com/r/${random}`);
+
+        message.channel.send(embed);
+    },
+    aliases: ['reddit', 'rdm'],
+    description: 'Memes direto do reddit, hehe'
+}
