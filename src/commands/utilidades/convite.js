316fbf1cf68841d49e19bd91dbf8f866a8fa5ae9
The-Crow-pleb <64552456+The-Crow-pleb@users.noreply.github.com>
2020-10-13 10:02:53 -0700

    Link de convite mudado(Permissões)

--- src/commands/utilidades/convite.js
+++ src/commands/utilidades/convite.js
@@ -5,7 +5,7 @@ module.exports = {
         if(message.author.bot) return;
         const { guild } = message
         const embed = new MessageEmbed()
-            .setDescription('Olá, [este](https://discord.com/oauth2/authorize?client_id=762077336812126228&scope=bot&permissions=837168254) é o convite que me fará entrar em seu servidor!')
+            .setDescription('Olá, [este](https://discord.com/oauth2/authorize?client_id=762077336812126228&scope=bot&permissions=2112351350) é o convite que me fará entrar em seu servidor!')
             .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
             .setColor('RANDOM')
             .setFooter('Sim! Eu preciso de bastante permissões para funcionar, não me culpe!')
