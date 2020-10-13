b1a79365b7c236f7f72a83a87529ca1a3106a62a
The-Crow-pleb <64552456+The-Crow-pleb@users.noreply.github.com>
2020-10-13 10:04:35 -0700

    Sincronização com a API em caso de permissão negada

--- src/events/members/guildMemberAdd.js
+++ src/events/members/guildMemberAdd.js
@@ -1,17 +1,22 @@
-const { MessageEmbed } = require("discord.js");
+module.exports = (client, member, message) => {
 
-module.exports = (client, member, message, data) => {
-    
-    const {MessageEmbed} = require('discord.js')
-    const channel = member.guild.channels.cache.find(x => x.name === 'bem-vindo')
-    if(!channel) return;
+    try{
+        const {MessageEmbed} = require('discord.js')
+        const channel = member.guild.channels.cache.find(x => x.name === 'bem-vindo')
+        if(!channel) return;
 
-    const embed = new MessageEmbed()
-        .setThumbnail(member.user.avatarURL())
-        .setTitle(`${member.user.tag} seja bem-vindo ao servidor!`)
-        .setDescription('Espero que goste de sua estadia aqui!')
-        .setColor('RANDOM')
-        .setFooter('Trate de ler as regras, caso não leia, no futuro você estará em meu forninho hehe')
-        .setImage('https://i.pinimg.com/originals/b0/de/02/b0de026a12e20137a654b5e2e65e2aed.gif')
-    channel.send(embed)
+        const botPerms = ['SEND_MESSAGES']
+        if(!channel.permissionsFor(client.user).has('SEND_MESSAGES')) return; //FINALMENTE, SEM ERRO DE API NO CONSOLE AAAAAAAAAAAAAAAAAAA
+        
+        const embed = new MessageEmbed()
+            .setThumbnail(member.user.avatarURL())
+            .setTitle(`${member.user.tag} seja bem-vindo ao servidor!`)
+            .setDescription('Espero que goste de sua estadia aqui!')
+            .setColor('RANDOM')
+            .setFooter('Trate de ler as regras, caso não leia, no futuro você estará em meu forninho hehe')
+            .setImage('https://i.pinimg.com/originals/b0/de/02/b0de026a12e20137a654b5e2e65e2aed.gif')
+        channel.send(embed)
+    }catch(err) {
+        console.log(err) //Para não logar o erro de API
+    }
 }
