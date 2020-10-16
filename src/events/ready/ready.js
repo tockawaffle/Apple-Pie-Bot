a9f6de4be13d5bb821f1f6d491ac53220c4fb64d
The-Crow-pleb <64552456+The-Crow-pleb@users.noreply.github.com>
2020-10-16 14:12:36 -0700

    Custom Status

--- src/events/ready/ready.js
+++ src/events/ready/ready.js
@@ -4,25 +4,26 @@ const { commandStatus, eventStatus } = require('../../util/registry');
 
 module.exports = async (client) => {
 
-    console.log(`${client.user.tag} has logged in.`);
+    console.log(`${client.user.tag} Saiu do forno!`);
     await loadTable(commandStatus, 50);
     console.log("\n");
     await loadTable(eventStatus, 50);
 
+    client.user.setActivity(`Atualmente, ${client.guilds.cache.size} servidores me acolheram como bot!`, {type: 'PLAYING'});
     let activNum = 0;
     setInterval(function() {
         if(activNum === 0) {
-            client.user.setActivity(`Atualmente, ${client.guilds.cache.size} servidores me acolheram como bot!`, {type: 'PLAYING'});
+            client.user.setActivity("O forno é uma ferramenta dificil de se usar, por isso nem todos conseguem!")
             activNum = 1;
         } else if (activNum === 1) {
-            client.user.setActivity("Torta é crocante, torta é vida, se eu fosse uma torta vc me comeria?")
+            client.user.setActivity("Torta é crocante, torta é vida, se eu fosse uma torta você me comeria?")
+            activNum = 2;
+        } else if (activNum === 2) {
+            client.user.setActivity("A vida é curta! Não deixe para amanhã a torta que você pode comer hoje.")
+            activNum = 3;
+        } else if (activNum === 3) {
+            client.user.setActivity(`O nome do meu criador é Tocka Waifu!`)
             activNum = 0;
-        // } else if (activNum === 2) {
-        //     client.user.setActivity("")
-        //     activNum = 3;
-        // } else if (activNum === 3) {
-        //     client.user.setActivity('')
-        //     activNum = 0;
         }  
     }, 300 * 1000);
 }
