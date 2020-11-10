6901ef3305e6d9048ca76d7438834ef0c96a2246
Marcello Brito <just4animes@gmail.com>
2020-11-10 19:42:59 -0300

    Toss a coin to your witcher~

---
+++ src/commands/diversão/coin.js
@@ -0,0 +1,33 @@
+const languages = require('../../languages/languages')
+
+module.exports = {
+    run: async(client, message, args) => {
+
+        const { guild } = message
+
+        const head = [`${languages(guild, 'COIN_C')}`, `${languages(guild, 'COIN_C2')}`]
+        const random = head[Math.floor(Math.random() * head.length)]
+        const rdmChoice = head[Math.floor(Math.random() * head.length)]
+
+        message.reply(`${languages(guild, 'COIN')}`).then(() => {
+            message.channel.awaitMessages(response => response.content === `${languages(guild, 'COIN_C')}` || response.content === `${languages(guild, 'COIN_C2')}`, {
+                max: 1,
+                time: 10000,
+                errors: ['time'],
+            }).then((collected) => {
+                let response = collected.first().content.toLowerCase()
+            if(response ==  `${languages(guild, 'COIN_C')}` && rdmChoice == `${languages(guild, 'COIN_C2')}`) {
+                    message.channel.send(`${languages(guild, 'COIN2')}\n${languages(guild, 'COIN3')} ${rdmChoice}`)
+                } else if( response == `${languages(guild, 'COIN_C2')}` && rdmChoice == `${languages(guild, 'COIN_C')}`) {
+                    message.channel.send(`${languages(guild, 'COIN2')}\n${languages(guild, 'COIN3')} ${rdmChoice}`)
+                } else if ( response === rdmChoice ) {
+                    message.channel.send(`${languages(guild, 'COIN4')}`)
+                }
+            }).catch(() => {
+            message.reply(`${languages(guild, 'COIN_ERR')}`)
+        })
+        })
+
+        console.log(random)
+    }, aliases: [''], description: ''
+}
