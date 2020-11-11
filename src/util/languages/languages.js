6cff872e0a748bc3eb5bfff13d2d24219a861c21
Marcello Brito <just4animes@gmail.com>
2020-11-11 18:31:00 -0300

    Changing the directory so it looks cleaner

---
+++ src/util/languages/languages.js
@@ -0,0 +1,38 @@
+const mongoose = require('../../../db/db')
+const langSchema = require('../../../db/schemas/language-schema')
+const languages = require('./languages.json')
+
+const guildLanguages = {}
+const loadLangs = async(client) => {
+    await mongoose.then(async mongoose => {
+        try {
+            for (const guild of client.guilds.cache) {
+                const guildId = guild[0]
+
+                const result = await langSchema.findOne({
+                    _id: guildId,
+                })
+                guildLanguages[guildId] = result ? result.language : 'portugues'
+            }
+        } catch(err) {
+            console.log(err)
+        }
+    })
+}
+
+const setLanguage = (guild, languages) => {
+    guildLanguages[guild.id] = languages
+}
+
+module.exports = (guild, textId) => {
+    if(!languages.traduções[textId]) {
+        throw new Error(`ID de texto não definido: ${textId}`)
+    }
+
+    const selectedLanguage = guildLanguages[guild.id]
+
+    return languages.traduções[textId][selectedLanguage]
+}
+
+module.exports.loadLangs = loadLangs
+module.exports.setLanguage = setLanguage
