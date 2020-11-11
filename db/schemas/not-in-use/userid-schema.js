03876fc6a49ae410db6ce3829741a8385e839fa5
Marcello Brito <just4animes@gmail.com>
2020-11-11 18:26:49 -0300

    Not using these schemas now

---
+++ db/schemas/not-in-use/userid-schema.js
@@ -0,0 +1,17 @@
+//Not in use, will not insert into database.
+
+const mongoose = require('mongoose')
+
+const userIdSchema = mongoose.Schema({
+  // The user ID
+  _id: {
+    type: String,
+    required: true,
+  },
+  messageCount: {
+    type: Number,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('users-ids', userIdSchema)
