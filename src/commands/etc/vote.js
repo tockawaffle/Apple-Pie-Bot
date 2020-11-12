487c9d4093f94aed67be70e078ae83dd9d7ad394
The-Crow-pleb <64552456+The-Crow-pleb@users.noreply.github.com>
2020-11-11 18:51:04 -0800

    just some quick fixes

--- src/commands/etc/vote.js
+++ src/commands/etc/vote.js
@@ -6,8 +6,18 @@ module.exports = {
         if(message.author.bot) return;
         const { guild } = message
         const embed = new MessageEmbed()
-            .setDescription(`[${languages(guild, 'VT_C')}](https://top.gg/bot/762077336812126228) ${languages(guild, 'VT_C2')}`)
+            .setDescription(`${languages(guild, 'VT_C')}`)
             .setAuthor(`${guild.name}`, guild.iconURL({ dynamic: true }))
+            .addFields(
+                {
+                    name: 'top.gg:',
+                    value: `[${languages(guild, 'VT_C2')}](https://top.gg/bot/762077336812126228)`
+                },
+                {
+                    name: 'Discord Bot List:',
+                    value: `[${languages(guild, 'VT_C2')}](https://discord.ly/apple-pie)`
+                }
+            )
             .setColor('RANDOM')
             .setFooter(`${languages(guild, 'VT_C3')}`)
         message.channel.send(embed)
