d41b8c0672bd80dc96a1b782a6e20fef33ed6884
Marcello Brito <just4animes@gmail.com>
2020-12-19 19:57:18 -0300

    Music Commands Changes

--- src/commands/music/filter.js
+++ src/commands/music/filter.js
@@ -19,12 +19,73 @@ module.exports = {
 
         if (!client.player.getQueue(message)) return message.reply(`${languages(guild, 'LPNQ')}`)
 
-
+        let dj = message.member.roles.cache.find(x => x.name === 'DJ')
+    
+        const track = await client.player.nowPlaying(message);
+        if(message.author.id !== track.requestedBy.id && !dj) {
+            const notRequestedBy = new MessageEmbed()
+                .setAuthor(guild.name, guild.iconURL({dynamic: true}))
+                .setDescription(`${languages(guild, "FF")}`)
+                .addFields(
+                    {
+                        name: `${languages(guild, "FF_2")}`,
+                        value: `\`\`\`${track.requestedBy.username}\`\`\``
+                    }
+                )
+                .setFooter(`${languages(guild, "FF_3")}`)
+                .setColor("RANDOM")
+            message.reply(notRequestedBy)
+            return
+        } else if(message.author.id !== track.requestedBy.id && dj) {
+            if (!args[0]) {
+                const noFilterFound = new MessageEmbed()
+                    .setColor('RANDOM')
+                    .setDescription(`${languages(guild, "FE")}`)
+                    .setAuthor(`${message.guid.name}`, message.guild.iconURL({dynamic: true}))
+                message.reply(noFilterFound)
+                return
+            }
+    
+            const filterToUpdate = Object.values(client.filters).find((f) => f.toLowerCase() === args[0].toLowerCase());
+        
+            if (!filterToUpdate) {
+                const noValidFilter = new MessageEmbed()
+                    .setColor('RANDOM')
+                    .setDescription(`${languages(guild, "FE_2")}`)
+                    .setAuthor(`${message.guild.name}`, message.guild.iconURL({dynamic: true}))
+                message.reply(noValidFilter)
+                return
+            }
+            const filterRealName = Object.keys(client.filters).find((f) => client.filters[f] === filterToUpdate);
+        
+            const queueFilters = client.player.getQueue(message).filters
+            const filtersUpdated = {};
+            filtersUpdated[filterRealName] = queueFilters[filterRealName] ? false : true;
+            client.player.setFilters(message, filtersUpdated);
+        
+            if (filtersUpdated[filterRealName]) {
+                const filterUpdated = new MessageEmbed()
+                    .setAuthor(`${message.guild.name}`, message.guild.iconURL({dynamic: true}))
+                    .setColor('RANDOM')
+                    .setDescription(`${languages(guild, "FE_3")}`)
+                    .addField(`${languages(guild, "FE_7")}`, `${languages(guild, "FE_4")}`)
+                message.reply(filterUpdated)
+            }
+            else {
+                const filterRemoved = new MessageEmbed()
+                    .setAuthor(`${message.guild.name}`, message.guild.iconURL({dynamic: true}))
+                    .setColor('RANDOM')
+                    .setDescription(`${languages(guild, "FE_5")}`)
+                    .addField(`${languages(guild, "FE_7")}`, `${languages(guild, "FE_6")}`)
+                    message.channel.send(filterRemoved);
+            }
+            return
+        }
 
         if (!args[0]) {
             const noFilterFound = new MessageEmbed()
                 .setColor('RANDOM')
-                .setDescription(`You need to provide one filter to enable/disable!`)
+                .setDescription(`${languages(guild, "FE")}`)
                 .setAuthor(`${message.guid.name}`, message.guild.iconURL({dynamic: true}))
             message.reply(noFilterFound)
             return
@@ -35,7 +96,7 @@ module.exports = {
         if (!filterToUpdate) {
             const noValidFilter = new MessageEmbed()
                 .setColor('RANDOM')
-                .setDescription(`That filter does not exists! Please, provide a valid filter.`)
+                .setDescription(`${languages(guild, "FE_2")}`)
                 .setAuthor(`${message.guild.name}`, message.guild.iconURL({dynamic: true}))
             message.reply(noValidFilter)
             return
--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -22,41 +22,9 @@ module.exports = {
             .setDescription(
                 `${languages(guild, "Q_2")} ${queue.playing.title} | ${queue.playing.author}\n\n` + (queue.tracks.map((track, i) => {
                 return `**#${i + 1}** - ${track.title}`
-                }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `${languages(guild, "Q_3")} **${queue.tracks.length}** ${languages(guild, "Q_4")}` : `${languages(guild, "Q_5")} **${queue.tracks.length}** ${languages(guild, Q_6)}`}`))
+                }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `${languages(guild, "Q_3")} **${queue.tracks.length}** ${languages(guild, "Q_4")}` : `${languages(guild, "Q_5")} **${queue.tracks.length}** ${languages(guild, "Q_6")}`}`))
             .setColor("RANDOM")
             .setFooter(client.user.username, client.user.avatarURL())
-        message.channel.send(queueEmbed).catch(err => {
-            const embedError = new MessageEmbed()
-                .setAuthor(guild.name, guild.iconURL({dynamic: true}))
-                .setDescription(`${languages(guild, "M_E")}`)
-                .addFields(
-                    {
-                        name: `${languages(guild, "M_E2")}`,
-                        value: `\`\`\`${err}\`\`\``
-                    },
-                    {
-                        name: `${languages(guild, "M_E3")}`,
-                        value: `${languages(guild, "M_E4")}`
-                    }
-                )
-            const solution = new MessageEmbed()
-                .setAuthor(guild.name, guild.iconURL({dynamic: true}))
-                .setDescription(`${languages(guild, "M_E5")}`)
-                .addFields(
-                    {
-                        name: `${languages(guild, "M_E6")}`,
-                        value: `[Click Here](https://www.applepiebot.xyz/permission-flags)`
-                    },
-                    {
-                        name: `${languages(guild, "M_E7")}`,
-                        value: `${languages(guild, "M_E8")}`
-                    }
-                )
-            pages = [
-                embedError,
-                solution
-            ]
-            pageEmbed(message,pages)
-        })
+        message.channel.send(queueEmbed)
     }
 }
