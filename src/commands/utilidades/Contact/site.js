a4161ec7c3d920d3bb133802a36a652166895cee
Marcello Brito <just4animes@gmail.com>
2020-12-29 05:43:49 -0300

    Tradução

--- src/commands/utilidades/Contact/site.js
+++ src/commands/utilidades/Contact/site.js
@@ -7,44 +7,46 @@ module.exports = {
     description: 'Meu site',
     run: async(client, message) => {
 
+        const {guild} = message
+
         const landingPage = new MessageEmbed()
             .setAuthor(message.guild.name, message.guild.iconURL({dynamic: true}))
-            .setDescription(`Thank you for visiting me ${process.env.CUTIE}`, process.env.CUTIE)
+            .setDescription(`${languages(guild, "ST")}` + process.env.CUTIE)
             .setColor("RANDOM")
             .addFields(
                 {
-                    name: `This is my site:`,
-                    value: `[Click Here](https://www.applepiebot.xyz/)`
+                    name: `${languages(guild, "ST2")}`,
+                    value: `[${languages(guild, "H_C21")}](https://www.applepiebot.xyz/)`
                 }
             )
         const commmandsPage = new MessageEmbed()
             .setAuthor(message.guild.name, message.guild.iconURL({dynamic: true}))
-            .setDescription(`Thank you for visiting me `, process.env.CUTIE)
+            .setDescription(`${languages(guild, "ST")}` + process.env.CUTIE)
             .setColor("RANDOM")
             .addFields(
                 {
-                    name: `These are my commands, take a look on them!:`,
-                    value: `[Click Here](https://www.applepiebot.xyz/commands)`
+                    name: `${languages(guild, "ST3")}`,
+                    value: `[${languages(guild, "H_C21")}](https://www.applepiebot.xyz/commands)`
                 }
             )
         const invitePage = new MessageEmbed()
             .setAuthor(message.guild.name, message.guild.iconURL({dynamic: true}))
-            .setDescription(`Thank you for visiting me ${process.env.CUTIE}`)
+            .setDescription(`${languages(guild, "ST")}${process.env.CUTIE}`)
             .setColor("RANDOM")
             .addFields(
                 {
-                    name: `Here you can find where to invite me:`,
-                    value: `[Click Here](https://www.applepiebot.xyz/invite-me)`
+                    name: `${languages(guild, "ST4")}`,
+                    value: `[${languages(guild, "H_C21")}](https://www.applepiebot.xyz/invite-me)`
                 }
             )
         const donatePage = new MessageEmbed()
             .setAuthor(message.guild.name, message.guild.iconURL({dynamic: true}))
-            .setDescription(`Want to create your own bot or donate to Apple?`, process.env.CUTIE)
+            .setDescription(`${languages(guild, "ST5")}` + process.env.CUTIE)
             .setColor("RANDOM")
             .addFields(
                 {
-                    name: `Here you can find where to do it!`,
-                    value: `[Click Here](https://www.applepiebot.xyz/donate) or [Here!](https://www.fiverr.com/share/2AGlEN) (if you want to buy a bot)`
+                    name: `${languages(guild, "ST6")}`,
+                    value: `[${languages(guild, "H_C21")}](https://www.applepiebot.xyz/donate)`
                 }
             )
         pages = [
