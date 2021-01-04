f2d736653e8f2b0cf1c26e997070a65640c2d089
Marcello Brito <just4animes@gmail.com>
2021-01-03 23:29:36 -0300

    Outros eventos + Traduções + QuickFixes

--- src/events/discordEvents/message/message.js
+++ src/events/discordEvents/message/message.js
@@ -1,6 +1,5 @@
 const PREFIX = process.env.PREFIX
 const {RateLimiter} = require('discord.js-rate-limiter')
-const languages = require('../../util/languages/languages')
 const ids = process.env.ID || process.env.ID2
 let rateLimiter = new RateLimiter(2, 2000)
 
@@ -11,6 +10,7 @@ module.exports = async(client, message) => {
 
     if(message.author.bot) return;
     if(message.channel.type === 'dm' && !ids) return
+    else if(message.channel.type === 'dm' && ids) return console.log(`${message.author.username}: ${message.content}`)
     if(!channel.permissionsFor(client.user).has('SEND_MESSAGES' && 'EMBED_LINKS')) return
 
     if(message.content.startsWith(PREFIX)) {
@@ -25,8 +25,6 @@ module.exports = async(client, message) => {
             if(client.commands.get(cmdName)) {
                 client.commands.get(cmdName)(client, message, cmdArgs)
             }else {
-                // message.reply(`${languages(guild, 'CMD_EV')}  **'-${cmdName}'** ${languages(guild, 'CMD_EV_2')}`);
-                // message.channel.send(process.env.NKCF);
                 return
             }
         }
