7ed3c2a43f420b425acf4b05c29952c095815520
Marcello Brito <just4animes@gmail.com>
2021-01-07 15:48:05 -0300

    Comandos refeitos

--- src/commands/utilidades/Users/avatar.js
+++ src/commands/utilidades/Users/avatar.js
@@ -7,16 +7,26 @@ module.exports = {
     run: async(client, message, args) => {
         const {guild} = message
         const member = message.mentions.members.first() || guild.members.cache.get(args[0])
-        if(!member) {
-            const avatar = message.author.avatarURL({dynamic: true, size: 2048, format: 'png'})
-            const authorAv = new MessageEmbed()
+        const gUser = client.users.cache.get(args[0])
+
+
+        if(gUser && !member) {
+            const avatar = gUser.avatarURL({dynamic: true, size: 2048, format: 'png'})
+            const avatar2 = message.author.avatarURL({dynamic: true, size: 2048, format: 'png'})
+            const gUserAv = new MessageEmbed()
                 .setAuthor(guild.name, guild.iconURL({dynamic: true}))
                 .setColor("RANDOM")
-                .setTitle(`🔍${message.author.username}`)
-                .setDescription(`[${languages(guild, "AVATAR_C")}](${avatar}) ${languages(guild, "AV_C")}`)
+                .setTitle(`🌎🔎 ${gUser.username}`)
+                .setDescription(`[${languages(guild, "AVATAR_C")}](${avatar}) ${languages(guild, "AV_C2")}`)
                 .setImage(await avatar)
-            message.reply(authorAv)
-            return
+            const authorAv = new MessageEmbed()
+                .setAuthor(guild.name, guild.iconURL({dynamic: true}))
+                .setTitle(`🔍${message.author.username}`)
+                .setColor("RANDOM")
+                .setDescription(`[${languages(guild, "AVATAR_C")}](${avatar2}) ${languages(guild, "AV_C")}`)
+                .setImage(await avatar2)
+            pages = [gUserAv, authorAv]
+            page(message, pages)    
         } else if(member){
             const avatar2 = message.author.avatarURL({dynamic: true, size: 2048, format: 'png'})
             const avatar = member.user.avatarURL({dynamic: true, size: 2048, format: 'png'})
@@ -32,11 +42,18 @@ module.exports = {
                 .setColor("RANDOM")
                 .setDescription(`[${languages(guild, "AVATAR_C")}](${avatar2}) ${languages(guild, "AV_C")}`)
                 .setImage(await avatar2)
-            pages = [
-                memberAv,
-                authorAv
-            ]
+            pages = [memberAv,authorAv]
             page(message, pages)
+        } else if(!member && !gUser) {
+            const avatar = message.author.avatarURL({dynamic: true, size: 2048, format: 'png'})
+            const authorAv = new MessageEmbed()
+                .setAuthor(guild.name, guild.iconURL({dynamic: true}))
+                .setColor("RANDOM")
+                .setTitle(`🔍${message.author.username}`)
+                .setDescription(`[${languages(guild, "AVATAR_C")}](${avatar}) ${languages(guild, "AV_C")}`)
+                .setImage(await avatar)
+            message.reply(authorAv)
+            return
         }
     }
 }
