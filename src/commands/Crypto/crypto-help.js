b92aef5c7d295dbd8d86205d6cca793717ce7800
The-Crow-Pleb <you@example.com>
2021-02-18 08:39:01 -0300

    Crypto-Wallets

--- src/commands/Crypto/crypto-help.js
+++ src/commands/Crypto/crypto-help.js
@@ -26,12 +26,12 @@ module.exports = {
             .setAuthor(guild.name, guild.iconURL({dynamic: true}))
             .setDescription('Consider Donating!')
             .addFields(
-                {name: '<:bitcoin:811435136678756402> Bitcoin Wallet:', value: '\`\`\`bc1qduxslynek8t59uzvlkkxv34a08ck3k9q545yqa\`\`\`'},
-                {name: '<:ethereum:811436739738075178> Ethereum Wallet:', value: '```0xdf5869e0c15bfcb9903919a0c37836c11b674b1a  -  Gas Limit: 36,000```'},
+                {name: '<:bitcoin:811435136678756402> Bitcoin Wallet:', value: '\`\`\`bc1qtgkxy2zcgvxyk9kwt9nhc4gzfyytrjzhc3q97y\`\`\`'},
+                {name: '<:ethereum:811436739738075178> Ethereum Wallet:', value: '```0x208f7243e8d0295dd023e9e102d54eb079bab2e1  -  Gas Limit: 36,000```'},
                 {name: '<:xrp:811433454209073192> XRP (Ripple) Wallet:', value: '```rnW8je5SsuFjkMSWkgfXvqZH3gLTpXxfFH  -  Destination Tag: 100549967```'},
                 {name: '<:litecoin:811437478446760056> Litecoin Wallet:', value: '```MRvTvzDwWbiVUARt4P1HSBR4g2FYEGx8g1```'},
                 {name: '<:ravencoin:811438538721067038> Ravencoin Wallet:', value: '```rLukKVTLwXyDE8CBAvnxsbbewKStWfeweR```'},
-                {name: '<:nanocoin:811416752026157066> Nano Coin Wallet:', value: '```nano_1t6tj3zqwjxsxda7o6wb6gzc65y6h1qifoysn9if9x8nj4bh7igbpao46szm```'},
+                {name: '<:nanocoin:811416752026157066> Nano Coin Wallet:', value: '```nano_1w5dof5cb6ss3aqubsweybkg6os4xtbisnwfqghogooswdhi37qt93umtmt8```'},
                 {name: '<:basicattentiontoken:811443748486512678> BAT (Basic Attention Token) Wallet:', value: '```0x8ebe9261111ff32a24fa311668d328724a309f96  -  Gas Limit: 36,000```'}
             )
         page = [CryptoHelp, CryptoDonate]
