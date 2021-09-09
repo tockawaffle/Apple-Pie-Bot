f59b0e4d75f3888d19c096d339d318575ba70eed
The-Crow-pleb <just4animes@gmail.com>
2021-09-09 02:51:30 -0300

    added images

--- src/events/discordEvents/ready/ready.js
+++ src/events/discordEvents/ready/ready.js
@@ -1,8 +1,17 @@
-const db = require("@db/db"); const {loadUserLangs} = require("@lang")
+const db = require("@db/db"); const {loadUserLangs} = require("@lang");
 module.exports = async(client) => {
 
     await db.then(console.log(`${client.user.username} - DataBase Connected.`))
     await loadUserLangs(client).then(console.log(`${client.user.username} - Languages Loaded`))
     console.log(`${client.user.username} - Ready.`)
     
+    const ms = require("ms")
+    client.user.setAvatar('src/Configs/Events/imgs/perfil/i1.png').catch(err => console.log(`${err}`)); let av = 0
+    setInterval(function() {
+        if(av === 0) {client.user.setAvatar('src/Configs/Events/imgs/perfil/i2.jpg'); av = 1} 
+        else if (av === 1) {client.user.setAvatar('src/Configs/Events/imgs/perfil/i3.jpg'); av = 2} 
+        else if (av === 2 ) {client.user.setAvatar('src/Configs/Events/imgs/perfil/i4.jpg'); av = 3} 
+        else if (av === 3) {client.user.setAvatar('src/Configs/Events/imgs/perfil/i5.jpg'); av = 4}
+        else if (av === 4) {client.user.setAvatar('src/Configs/Events/imgs/perfil/i1'); av = 0} 
+    }, ms('2h'));
 }
